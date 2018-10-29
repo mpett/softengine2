c5c63d608a9a2695f8e8bfd01eb2f3b19c1692ba
Martin Pettersson <kontakt@martinpettersson.se>
2018-10-29 18:07:59 +0100

    Copy paste reference

--- softengine.ts
+++ softengine.ts
@@ -10,7 +10,6 @@ module SoftEngine {
             this.Target = BABYLON.Vector3.Zero();
         }
     }
-
     export class Mesh {
         Position: BABYLON.Vector3;
         Rotation: BABYLON.Vector3;
@@ -24,13 +23,15 @@ module SoftEngine {
     }
 
     export class Device {
-        // The backbuffer is equal to the number of pixels to draw on the screen.
+        // the back buffer size is equal to the number of pixels to draw
+        // on screen (width*height) * 4 (R,G,B & Alpha values). 
         private backbuffer: ImageData;
         private workingCanvas: HTMLCanvasElement;
         private workingContext: CanvasRenderingContext2D;
         private workingWidth: number;
         private workingHeight: number;
-        private backbufferdata;
+        // equals to backbuffer.data
+        private backbufferdata; 
 
         constructor(canvas: HTMLCanvasElement) {
             this.workingCanvas = canvas;
@@ -39,67 +40,80 @@ module SoftEngine {
             this.workingContext = this.workingCanvas.getContext("2d");
         }
 
-        // Clear the backbuffer with a specific color.
+        // This function is called to clear the back buffer with a specific color
         public clear(): void {
-            // Clear with black color by default.
+            // Clearing with black color by default
             this.workingContext.clearRect(0, 0, this.workingWidth, this.workingHeight);
-            // Once cleared, clear back buffer data
+            // once cleared with black pixels, we're getting back the associated image data to 
+            // clear out back buffer
             this.backbuffer = this.workingContext.getImageData(0, 0, this.workingWidth, this.workingHeight);
         }
 
-        // Once ready, we can flush the back buffer into the front buffer.
+        // Once everything is ready, we can flush the back buffer
+        // into the front buffer. 
         public present(): void {
             this.workingContext.putImageData(this.backbuffer, 0, 0);
         }
 
-        // Called to put a pixel on the screen with specific x,y - coordinates
+        // Called to put a pixel on screen at a specific X,Y coordinates
         public putPixel(x: number, y: number, color: BABYLON.Color4): void {
             this.backbufferdata = this.backbuffer.data;
-            var index:number = ((x >> 0) + (y >> 0) * this.workingWidth) * 4;
+            // As we have a 1-D Array for our back buffer
+            // we need to know the equivalent cell index in 1-D based
+            // on the 2D coordinates of the screen
+            var index: number = ((x >> 0) + (y >> 0) * this.workingWidth) * 4;
+
+            // RGBA color space is used by the HTML5 canvas
             this.backbufferdata[index] = color.r * 255;
             this.backbufferdata[index + 1] = color.g * 255;
             this.backbufferdata[index + 2] = color.b * 255;
             this.backbufferdata[index + 3] = color.a * 255;
         }
 
-        // Takes some 3D coordinates and transforms them into 2D coordinates using the 
-        // transformation matrix.
+        // Project takes some 3D coordinates and transform them
+        // in 2D coordinates using the transformation matrix
         public project(coord: BABYLON.Vector3, transMat: BABYLON.Matrix): BABYLON.Vector2 {
-            // Transforming the coordinates.
+            // transforming the coordinates
             var point = BABYLON.Vector3.TransformCoordinates(coord, transMat);
-            // Transformed coordinates will be based in the center of the screen.
-            // We need to transform them to have the origin in the top left.
+            // The transformed coordinates will be based on coordinate system
+            // starting on the center of the screen. But drawing on screen normally starts
+            // from top left. We then need to transform them again to have x:0, y:0 on top left.
             var x = point.x * this.workingWidth + this.workingWidth / 2.0 >> 0;
-            var y = point.y = this.workingHeight + this.workingHeight / 2.0 >> 0;
-            return (new BABYLON.Vector2(x,y));
+            var y = -point.y * this.workingHeight + this.workingHeight / 2.0 >> 0;
+            return (new BABYLON.Vector2(x, y));
         }
 
-        // Calls putPixel but does the clipping operation before.
+        // drawPoint calls putPixel but does the clipping operation before
         public drawPoint(point: BABYLON.Vector2): void {
-            // Clipping what is visible on the screen.
-            if (point.x > 0 && point.y > 0 && point.x < this.workingWidth && point.y < this.workingHeight) {
-                // Draw a yellow point.
-                this.putPixel(point.x, point.y, new BABYLON.Color4(1,1,0,1));
+            // Clipping what's visible on screen
+            if (point.x >= 0 && point.y >= 0 && point.x < this.workingWidth && point.y < this.workingHeight) {
+                // Drawing a yellow point
+                this.putPixel(point.x, point.y, new BABYLON.Color4(1, 1, 0, 1));
             }
         }
 
-        // The main method of the engine that re-computes each vertex projection during each frame.
+        // The main method of the engine that re-compute each vertex projection
+        // during each frame
         public render(camera: Camera, meshes: Mesh[]): void {
+            // To understand this part, please read the prerequisites resources
             var viewMatrix = BABYLON.Matrix.LookAtLH(camera.Position, camera.Target, BABYLON.Vector3.Up());
             var projectionMatrix = BABYLON.Matrix.PerspectiveFovLH(0.78, this.workingWidth / this.workingHeight, 0.01, 1.0);
 
             for (var index = 0; index < meshes.length; index++) {
-                // Current mesh to work on.
+                // current mesh to work on
                 var cMesh = meshes[index];
-                // Apply rotation before translation.
-                var worldMatrix = BABYLON.Matrix.RotationYawPitchRoll(cMesh.Rotation.y, cMesh.Rotation.x, cMesh.Rotation.z)
-                                    .multiply(BABYLON.Matrix.Translation(cMesh.Position.x, cMesh.Position.y, cMesh.Position.z));
+                // Beware to apply rotation before translation
+                var worldMatrix = BABYLON.Matrix.RotationYawPitchRoll(
+                        cMesh.Rotation.y, cMesh.Rotation.x, cMesh.Rotation.z)
+                        .multiply(BABYLON.Matrix.Translation(
+                            cMesh.Position.x, cMesh.Position.y, cMesh.Position.z));
+
                 var transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
 
                 for (var indexVertices = 0; indexVertices < cMesh.Vertices.length; indexVertices++) {
-                    // First, we project a 3D coordinate into 2D space
+                    // First, we project the 3D coordinates into the 2D space
                     var projectedPoint = this.project(cMesh.Vertices[indexVertices], transformMatrix);
-                    // Then draw it on the screen.
+                    // Then we can draw on screen
                     this.drawPoint(projectedPoint);
                 }
             }
